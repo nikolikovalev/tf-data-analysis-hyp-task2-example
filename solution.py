d2e612c72b593b782312bf8350b4a0987d7f8b83
Nikolay <120409996+nikolikovalev@users.noreply.github.com>
2023-04-13 20:52:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import anderson_ksamp
+from scipy.stats import mannwhitneyu
 
 
 chat_id = 411809593 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     a = 0.06
-    res = anderson_ksamp([x, y])
+    res = mannwhitneyu(x, y)
     return res.pvalue < a
